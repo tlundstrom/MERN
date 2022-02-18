6878db9b685faa71fa288e05066fe1934a96e66c
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-02-17 19:28:07 -0800

    Putting it together

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,12 +1,22 @@
-import react from "react";
+import react, {useState} from "react";
 
 const PersonCard = (props) =>{
+    const [state, setState] = useState({
+        currantAge: props.age
+    });
+
+    const handleClick = () => {
+        setState({
+            currantAge: state.currantAge + 1
+        });
+    }
     
     return(
         <div>
             <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {state.currantAge}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={handleClick}>Birthday Button for  {props.firstName} {props.lastName}</button>
         </div>
     )
 }
