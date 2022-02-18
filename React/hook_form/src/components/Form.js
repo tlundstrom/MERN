0817eebb3c4a94c4214962b0a3c4fbf8614da945
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-02-17 21:16:46 -0800

    More Forms

--- React/hook_form/src/components/Form.js
+++ React/hook_form/src/components/Form.js
@@ -14,14 +14,34 @@ const Form = (props) => {
             <form>
                 <p>First Name:</p>
                 <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
+                {
+                    firstName.length < 2 && firstName.length>0?
+                    <p>First Name must be at least than 2 characters.</p>:
+                    null
+                }
                 <p>Last Name:</p>
                 <input type="text" onChange={(e) => setLastName(e.target.value)} value={lastName}/>
+                {
+                    lastName.length < 2 && lastName.length>0?
+                    <p>Last Name must be at least than 2 characters.</p>:
+                    null
+                }
                 <p>Email:</p>
+                {
+                    email.length < 5 && email.length>0?
+                    <p>First Name must be at least than 5 characters.</p>:
+                    null
+                }
                 <input type="text" onChange={(e) => setEmail(e.target.value)} value={email}/>
                 <p>Password:</p>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} alue={password}/>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
                 <p>Confirm Password:</p>
-                <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} alue={confirmPassword}/>
+                <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/>
+                {
+                    confirmPassword !== password && confirmPassword.length >0?
+                    <p>Passwords must match</p>:
+                    null
+                }
             </form>
             <p>Your Form Data</p>
             <p>First Name: {firstName}</p>
