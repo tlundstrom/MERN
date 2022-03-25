959611d893c58b6d9122b2c0cce0ead99d5f6d63
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-25 16:49:40 -0700

    added State and method to class component

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,25 +1,34 @@
 import React, {Component} from 'react'
 
 class PersonCard extends React.Component{
-    handleClick(age){ 
-        age =age+1;
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    handleClick = () => { 
+        
+        let currentAge = this.state.age + 1;
+        console.log(currentAge);
+        this.setState({
+            age: currentAge
+        })
     }
 
     render(){
         
-        const {firstName, lastName, hairColor, age} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return(
             <div>
                 <h2>{lastName}, {firstName}</h2>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
-                <button onClick={this.handleClick(age)}>Birthday Button for  {firstName} {lastName}</button>
+                <button onClick={this.handleClick}>Birthday Button for  {firstName} {lastName}</button>
             </div>
         )
     }
-        
-    
-    
 }
 
 export default PersonCard
