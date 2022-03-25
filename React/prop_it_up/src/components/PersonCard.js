2a4b00da9f6ac6ec5dfb35a69843c533c9ee978f
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-25 09:21:54 -0700

    converted to class component

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,24 +1,25 @@
-import react, {useState} from "react";
+import React, {Component} from 'react'
 
-const PersonCard = (props) =>{
-    const [state, setState] = useState({
-        currantAge: props.age
-    });
-
-    const handleClick = () => {
-        setState({
-            currantAge: state.currantAge + 1
-        });
+class PersonCard extends React.Component{
+    handleClick(age){ 
+        age =age+1;
     }
+
+    render(){
+        
+        const {firstName, lastName, hairColor, age} = this.props;
+        return(
+            <div>
+                <h2>{lastName}, {firstName}</h2>
+                <p>Age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={this.handleClick(age)}>Birthday Button for  {firstName} {lastName}</button>
+            </div>
+        )
+    }
+        
     
-    return(
-        <div>
-            <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {state.currantAge}</p>
-            <p>Hair Color: {props.hairColor}</p>
-            <button onClick={handleClick}>Birthday Button for  {props.firstName} {props.lastName}</button>
-        </div>
-    )
+    
 }
 
 export default PersonCard
